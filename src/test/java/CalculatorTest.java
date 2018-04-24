2842984db877e70fe42dbb0f1939899b70422834
ufukdogdu07 <ufuk.dogdu@accenture.com>
2018-04-24 10:53:12 +0300

    test

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -22,7 +22,7 @@ public class CalculatorTest {
         calculator.setNumber1(5);
         calculator.setNumber2(3);
         int sum = calculator.getTotal();
-        assertEquals(8, sum);
+        assertEquals(2, sum);
     }
 
     @Test
