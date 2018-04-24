9ae8c6d35f8f8f20dbf9e556bff1416bc584c045
ufukdogdu07 <ufuk.dogdu@accenture.com>
2018-04-24 10:34:29 +0300

    Ufuk TEST

--- src/com/accenture/HelloController.java
+++ src/com/accenture/HelloController.java
@@ -10,7 +10,7 @@ import org.springframework.ui.ModelMap;
 public class HelloController {
     @RequestMapping(method = RequestMethod.GET)
     public String printHello(ModelMap model) {
-        model.addAttribute("message", "Merhaba ben Ufuk Doðdu 4455345453");
+        model.addAttribute("message", "Merhaba ben Ufuk Doðdu");
         return "hello";
     }
 }
